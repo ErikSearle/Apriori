f61a26d5cc3653a1582e4f06c395e99db1cc2b5e
Erik Searle <esearle@unbc.ca>
2018-10-01 08:52:13 -0700

    Implemented Apriori Algorithm

--- src/com/company/Apriori.java
+++ src/com/company/Apriori.java
@@ -1,4 +1,52 @@
 package com.company;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
 public class Apriori {
+
+    /**
+     *
+     * @param transactions The set of transactions from the database to test against (The mountain to mine)
+     * @param itemSet The set of items to test against the transaction database (The precious metal)
+     * @param minimumThreshold A number indicating the minimum number of times a set from within the item set must
+     *                         appear in the transactions to count as a frequentPattern. Must be a discrete number, not a
+     *                         a percentage value
+     * @return The set of frequent patterns found through recursive analysis
+     */
+    public static LinkedHashSet<TreeSet<Integer>> generateFrequentPatterns(LinkedHashSet<TreeSet<Integer>> transactions,
+                                                                           LinkedHashSet<TreeSet<Integer>> itemSet,
+                                                                           int minimumThreshold){
+        LinkedHashSet<TreeSet<Integer>> frequentPatterns = new LinkedHashSet<>();
+        /*
+        Test each item in the item set against the list of transactions and at the end of run through the transactions
+        list, if the number of hits is greater than the minimum threshold, add that set of items to a new item set
+        called frequentPatterns
+         */
+        for(TreeSet<Integer> item : itemSet){
+            int count = 0;
+            for(TreeSet<Integer> transaction : transactions){
+                if(transaction.containsAll(item)) count++;
+            }
+            if(count > minimumThreshold) frequentPatterns.add(item);
+        }
+
+        /*
+        Generate new candidates based on the frequentPatterns
+         */
+        LinkedHashSet<TreeSet<Integer>> newCandidates = CandidateGenerator.generate(frequentPatterns);
+
+        /*
+        If there are no new candidates, return the frequentPatterns only
+         */
+        if(newCandidates.isEmpty()) return frequentPatterns;
+
+        /*
+        Else recursively call this function with the new set of candidates. Add the frequentPatterns to the set returned
+        from the recursive call
+         */
+        frequentPatterns.addAll(generateFrequentPatterns(transactions, newCandidates, minimumThreshold));
+        return frequentPatterns;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,77 @@
 package com.company;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.function.Consumer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String inputFile = args[0];
+	    String minimumThreshold = args[1];
+	    Scanner scan = null;
+
+	    try {
+            scan = new Scanner(new File(inputFile));
+        } catch(IOException e){
+	        System.out.println("Unable to read from file");
+	        System.exit(1);
+        }
+        int numTransactions = scan.nextInt();
+	    int numItems = scan.nextInt();
+
+        LinkedHashSet<TreeSet<Integer>> transactionData = new LinkedHashSet<>();
+	    while(scan.hasNextLine()){
+	        String line = scan.nextLine();
+	        String[] lineData = line.split(" ");
+	        TreeSet<Integer> transaction = new TreeSet<>();
+	        for(String datum : lineData){
+	            if(!datum.equals("")) transaction.add(Integer.valueOf(datum));
+            }
+            transactionData.add(transaction);
+        }
+
+        LinkedHashSet<TreeSet<Integer>> itemSet = new LinkedHashSet<>();
+	    for(int i=0; i<numItems; i++){
+	        TreeSet<Integer> item = new TreeSet<>();
+	        item.add(i);
+	        itemSet.add(item);
+        }
+
+        LinkedHashSet<TreeSet<Integer>> frequentPatterns =
+                Apriori.generateFrequentPatterns(transactionData, itemSet,
+                        processMinThreshold(minimumThreshold, numTransactions));
+
+        frequentPatterns.forEach(new Consumer<TreeSet<Integer>>() {
+            @Override
+            public void accept(TreeSet<Integer> integers) {
+                StringBuilder sb = new StringBuilder();
+                for(Integer integer : integers){
+                    sb.append(integer).append(" ");
+                }
+                System.out.println(sb.toString());
+            }
+        });
+    }
+
+    private static int processMinThreshold(String minThresh, int numTransactions){
+        if(minThresh.charAt(minThresh.length()-1) == '%'){
+            String minThreshNumbersOnly = minThresh.substring(0, minThresh.length()-1);
+            float minThreshPercentage = Float.valueOf(minThreshNumbersOnly);
+            if(minThreshPercentage > 100 || minThreshPercentage < 0){
+                System.out.println("Invalid percentage. Percentage must be between 0 and 100.");
+                System.exit(1);
+            }
+            return (int) (((float) numTransactions)*(minThreshPercentage/100));
+        }
+        int minThreshNumber = Integer.valueOf(minThresh);
+        if(minThreshNumber < 0 || minThreshNumber > numTransactions){
+            System.out.println("Invalid minimum threshold. Threshold must be betwen 0 and " + numTransactions);
+            System.exit(1);
+        }
+        return minThreshNumber;
     }
 }
